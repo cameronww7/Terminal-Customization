3943c4739e46d69f451c737d7480d610bf791ba0
cameronww7 <cameronww7@gmail.com>
2021-02-20 19:03:14 -0800

    remove header and log file

--- terminal_setup.sh
+++ terminal_setup.sh
@@ -1,49 +1,94 @@
-#!/usr/bin/env zsh
-
 # Note
 # ---------------------------------------
 # Make sure file has needed perms
-# chmod +x terminal_setup.sh
+# sudo chmod +x terminal_setup.sh
 
-
+# Create Log FIle
+# ---------------------------------------
+sudo touch terminal_setup_logs.txt
 
 # Update the System
 # ---------------------------------------
+echo "\n apt update"
+echo "\n apt update" >> terminal_setup_logs.txt
 sudo apt update
 
+# Install zsh
+# ---------------------------------------
+echo "\n Installing - zsh"
+echo "\n Installing - zsh" >> terminal_setup_logs.txt
+sudo apt install -y zsh
+
+echo "\n echo $SHELL"
+echo "\n echo $SHELL" >> terminal_setup_logs.txt
+echo $SHELL
+echo $SHELL >> terminal_setup_logs.txt
+
+chsh -s $(which zsh) 
+chsh -s /usr/bin/zsh
 
 # Terminal Tools
 # ---------------------------------------
+echo "\n Installing - gedit"
+echo "\n Installing - gedit" >> terminal_setup_logs.txt
+sudo apt install -y gedit
+
+echo "\n Installing - terminator"
+echo "\n Installing - terminator" >> terminal_setup_logs.txt
 sudo apt install -y terminator
+
+echo "\n Installing - autojump"
+echo "\n Installing - autojump" >> terminal_setup_logs.txt
 sudo apt install -y autojump
+
+echo "\n Installing - tree"
+echo "\n Installing - tree" >> terminal_setup_logs.txt
 sudo apt install -y tree
+
+echo "\n Installing - acpi"
+echo "\n Installing - acpi" >> terminal_setup_logs.txt
 sudo apt install -y acpi
+
+echo "\n Installing - git"
+echo "\n Installing - git" >> terminal_setup_logs.txt
 sudo apt install -y git
 
 
 # Install Oh-My_ZSH
 # ---------------------------------------
 # Install oh-my-zsh
+echo "\n Installing - oh-my-zsh"
+echo "\n Installing - oh-my-zsh" >> terminal_setup_logs.txt
 sudo curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
 
 
 # Install Plugins
 # ---------------------------------------
 # add highlighting
+echo "\n Installing - highlighting"
+echo "\n Installing - highlighting" >> terminal_setup_logs.txt
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 # add auto-suggester
+echo "\n Installing - auto-suggester"
+echo "\n Installing - auto-suggester" >> terminal_setup_logs.txt
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 
 # install k
+echo "\n Installing - k"
+echo "\n Installing - k" >> terminal_setup_logs.txt
 sudo git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/plugins/k
 
 # install powerlevel9k
+echo "\n Installing - powerlevel9k"
+echo "\n Installing - powerlevel9k" >> terminal_setup_logs.txt
 sudo git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
 
 
 
 # Install .zshrc file 
+echo "\n Installing - .zshrc"
+echo "\n Installing - .zshrc" >> terminal_setup_logs.txt
 # ---------------------------------------
 sudo wget https://github.com/cameronww7/Terminal-Customization /opt/
 
