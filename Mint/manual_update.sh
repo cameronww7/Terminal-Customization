7f2bd96851431b1cd627eb82b19175ea304717cf
cameronww7 <cameronww7@gmail.com>
2021-03-24 19:23:21 -0700

    Syntax Issues fixed

--- Mint/manual_update.sh
+++ Mint/manual_update.sh
@@ -7,7 +7,7 @@
 echo "\n Updating - Terminal-Customization"
 echo "\n Updating - Terminal-Customization" 
 cd /opt/Terminal-Customization
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 
@@ -16,31 +16,31 @@ sudo git pull
 # Updating highlighting
 echo "\n Updating - zsh-syntax-highlighting"
 cd /home/$USER/.oh-my-zsh/plugins/zsh-syntax-highlighting
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 # Updating auto-suggester
 echo "\n Updating - zsh-autosuggestions"
 cd /home/$USER/.oh-my-zsh/plugins/zsh-autosuggestions
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 # Updating k
 echo "\n Updating - k"
 cd /home/$USER/.oh-my-zsh/plugins/k
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 # Updating powerlevel9k
 echo "\n Updating - powerlevel9k"
 cd /home/$USER/.oh-my-zsh/custom/themes/powerlevel9k
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 # Updating tmux-logging
 echo "\n Updating - tmux-logging"
 cd /opt/tmux-logging/
-sudo git fetch -A 
+sudo git fetch -a
 sudo git pull 
 
 # Updating .zshrc file 
