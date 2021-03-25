6636a4db52f5c0aebc1548b8b0127c1f7e4f93dc
cameronww7 <cameronww7@gmail.com>
2021-03-24 19:17:32 -0700

    some fixes!

--- Mint/Mint_terminal_setup_part_two.sh
+++ Mint/Mint_terminal_setup_part_two.sh
@@ -24,14 +24,14 @@ sudo git clone https://github.com/bhilburn/powerlevel9k.git /home/$USER/.oh-my-z
 # Install .zshrc file 
 # ---------------------------------------
 echo "\n\n\n Installing - .zshrc file \n"
-sudo cat /opt/Terminal-Customization/Kali/.zshrc > ~/.zshrc
+sudo cat /opt/Terminal-Customization/Mint/.zshrc > ~/.zshrc
 sudo source ~/.zshrc
 sudo chsh -s $(which zsh) $USER
 
 # Install .tmux.conf file 
 # ---------------------------------------
 echo "\n\n\n Installing - .tmux.conf file \n"
-sudo cat /opt/Terminal-Customization/Kali/.tmux.conf > ~/.tmux.conf
+sudo cat /opt/Terminal-Customization/Mint/.tmux.conf > ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
 # Setup my File Strucutres 
