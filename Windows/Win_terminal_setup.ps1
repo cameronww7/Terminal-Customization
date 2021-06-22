14556eb056f827f09e13c3fada1efd98302a7b01
cameronww7 <cameronww7@gmail.com>
2021-06-21 20:57:41 -0500

    Building Windows PS Setup Terminal Script

---
+++ Windows/Win_terminal_setup.ps1
@@ -0,0 +1,8 @@
+
+# Install Oh My Posh -https://docs.microsoft.com/en-us/windows/terminal/tutorials/powerline-setup
+# Fonts - https://github.com/microsoft/cascadia-code/releases
+# How to Install Fonts - https://windowsloop.com/download-install-cascadia-code-font/
+# Vid on how to - https://www.youtube.com/watch?v=lu__oGZVT98&t=120s
+
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
